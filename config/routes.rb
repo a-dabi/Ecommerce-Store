3912363b72098e1f9abbf22993181c7ff0e6269e
a-dabi <adrian.dabi@gmail.com>
2025-06-03 14:39:59 -0700

    re edit 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "simple_pages#index"
   get "simple_pages/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- get "simple_pages/about", to: "simple_pages#about", as: "simple_pages_about"
+get "simple_pages/about", to: "simple_pages#about", as: "simple_pages_about"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
